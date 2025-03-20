f9e85d29c8d709cfbb727cb92268028c565b176a
Matt Reisdorf Personal <mreisdorf9717@gmail.com>
2025-03-20 17:46:04 -0400

    first commit

---
+++ src/test/java/com/mattreisdorf/spotify_playlist_comparison_backend/SpotifyPlaylistComparisonBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mattreisdorf.spotify_playlist_comparison_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyPlaylistComparisonBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

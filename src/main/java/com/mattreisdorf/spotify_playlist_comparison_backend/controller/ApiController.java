955a2d9969f41aa42eb478b9a516cbd267c0ca29
Matt Reisdorf <mattreisdorf@MacBookPro.attlocal.net>
2025-03-25 15:58:40 -0400

    get playlist data endpoint -> need pagination for playlists >100 songs

--- src/main/java/com/mattreisdorf/spotify_playlist_comparison_backend/controller/ApiController.java
+++ src/main/java/com/mattreisdorf/spotify_playlist_comparison_backend/controller/ApiController.java
@@ -1,13 +1,19 @@
 package com.mattreisdorf.spotify_playlist_comparison_backend.controller;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.catalina.connector.Response;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -17,8 +23,9 @@ import jakarta.servlet.http.HttpSession;
 @CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
 @RestController
 public class ApiController {
-  
+
   private final String SPOTIFY_USER_URL = "https://api.spotify.com/v1/me";
+  private final String SPOTIFY_PLAYLIST_URL = "https://api.spotify.com/v1/playlists/";
 
   @GetMapping("/api/user")
   public ResponseEntity<String> getUserData(HttpServletRequest request) {
@@ -26,54 +33,61 @@ public class ApiController {
     HttpSession session = request.getSession(false);
 
     if (session == null || session.getAttribute("access_token") == null) {
-      System.out.println();
-      System.out.println("Session is either null or access_token is null");
-      System.out.println();
       return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Not Authorized");
     }
-
-    System.out.println();
-    System.out.println(session.getAttribute("access_token"));
-    System.out.println();
 
     String accessToken = (String) session.getAttribute("access_token");
 
     HttpHeaders headers = new HttpHeaders();
-    // headers.set("Authorization", "Bearer " + accessToken);
     headers.setBearerAuth(accessToken);
 
     HttpEntity<String> entity = new HttpEntity<>(headers);
 
     RestTemplate restTemplate = new RestTemplate();
-    ResponseEntity<String> spotifyResponse = restTemplate.exchange(
-      SPOTIFY_USER_URL,
-      HttpMethod.GET,
-      entity,
-      String.class
-    );
+    ResponseEntity<String> userDataResponse = restTemplate.exchange(
+        SPOTIFY_USER_URL,
+        HttpMethod.GET,
+        entity,
+        String.class);
 
-    return ResponseEntity.status(spotifyResponse.getStatusCode()).body(spotifyResponse.getBody());
+    return ResponseEntity.status(userDataResponse.getStatusCode()).body(userDataResponse.getBody());
   }
 
-  // @GetMapping("/api/playlist/{playlist_id}")
-  // public ResponseEntity<String> getPlaylistData(HttpServletRequest request, @PathVariable String playlistId) {
-  //   HttpSession session = request.getSession(false);
+  @GetMapping("/api/playlist")
+  public ResponseEntity<String> getPlaylistData(
+      HttpServletRequest request,
+      @RequestParam("playlist") String playlistUrl) {
 
-  //   if (session == null || session.getAttribute("access_token") == null) {
-  //     return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Not Authorized");
-  //   }
+    String urlRegex = "spotify\\.com/playlist/([^/?]+)";
+    Pattern pattern = Pattern.compile(urlRegex);
+    Matcher matcher = pattern.matcher(playlistUrl);
 
-  //   String accessToken = (String) session.getAttribute("access_token");
+    HttpSession session = request.getSession(false);
 
-  //   HttpHeaders headers = new HttpHeaders();
-  //   headers.setBearerAuth(accessToken);
+    if (session == null || session.getAttribute("access_token") == null) {
+      return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Not Authorized");
+    }
 
-  //   HttpEntity<String> entity = new HttpEntity<>(headers);
+    String accessToken = (String) session.getAttribute("access_token");
 
-  //   RestTemplate restTemplate = new RestTemplate();
+    HttpHeaders headers = new HttpHeaders();
+    headers.setBearerAuth(accessToken);
 
-  //   ResponseEntity<String> spotifyReponse = restTemplate.exchange(
+    HttpEntity<String> entity = new HttpEntity<>(headers);
 
-  //   )
-  // }
+    if (matcher.find()) {
+      String playlistId = matcher.group(1);
+
+      RestTemplate restTemplate = new RestTemplate();
+      ResponseEntity<String> playlistDataResponse = restTemplate.exchange(
+          SPOTIFY_PLAYLIST_URL + playlistId,
+          HttpMethod.GET,
+          entity,
+          String.class);
+      return ResponseEntity.status(playlistDataResponse.getStatusCode()).body(playlistDataResponse.getBody());
+    }
+
+    return new ResponseEntity<>("Invalid Playlist", HttpStatus.OK);
+
+  }
 }
